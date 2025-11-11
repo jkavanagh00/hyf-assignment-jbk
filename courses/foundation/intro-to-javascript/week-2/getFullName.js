cdff2820a0bc5ba15980f42b0fa9df57b28de693
jkavanagh00 <jkavanagh00@gmail.com>
2025-11-11 19:12:41 +0100

    added getFullName.js

---
+++ courses/foundation/intro-to-javascript/week-2/getFullName.js
@@ -0,0 +1,43 @@
+const honorifics = {
+    man: "Mr.",
+    woman: "Ms.",
+    "non-binary": "Mx."
+}
+
+const passengers = [
+    {
+        firstName: "John",
+        secondName: "Smith",
+        gender: "man",
+        formal: false
+    },
+    {
+        firstName: "Jane",
+        secondName: "Smythe",
+        gender: "woman",
+        formal: true
+    },
+    {
+        firstName: "Jayden",
+        secondName: "Smith",
+        gender: "non-binary",
+        formal: true
+    }
+]
+
+let formal = false;
+let gender = "man";
+
+function getFullName(firstName, secondName) {
+    var passenger = passengers.find(obj => {
+        return obj.firstName === firstName && obj.secondName === secondName;
+    });
+    
+    if (!passenger) {return "No passenger found"}
+    return passenger.formal ? `${honorifics[passenger.gender]} ${passenger.firstName} ${passenger.secondName}` : `${passenger.firstName} ${passenger.secondName}`
+}
+
+console.log(getFullName("John", "Smith"));
+console.log(getFullName("Jane", "Smythe"));
+console.log(getFullName("Jayden", "Smith"));
+console.log(getFullName("Jane", "Smith"));
