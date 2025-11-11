c638fa616993982bbf5f9dae0cb08323f48c4127
jkavanagh00 <jkavanagh00@gmail.com>
2025-11-11 21:17:25 +0100

    refactored studentManager.js to meet all requirements

--- courses/foundation/intro-to-javascript/week-2/studentManager.js
+++ courses/foundation/intro-to-javascript/week-2/studentManager.js
@@ -1,11 +1,22 @@
 const class07Students = [];
 
-const addStudentToClass = studentName => class07Students.push(studentName);
+function addStudentToClass (studentName) {
+    return studentName.length < 1 ?
+    "Invalid name entered" :
+
+    studentName === "The Queen" ?
+    class07Students.push(studentName) :
+
+    getNumberOfStudents() < 7 && !class07Students.includes(studentName) ? 
+    class07Students.push(studentName) :
+
+    getNumberOfStudents() > 6 ? 
+    "Cannot add more students to class 07" :
+
+    class07Students.includes(studentName) ?
+    `Student ${studentName} is already in the class` :
+
+    "Invalid name entered";
+};
 
 const  getNumberOfStudents = () => class07Students.length;
-
-console.log(getNumberOfStudents());
-addStudentToClass("Jenny");
-console.log(getNumberOfStudents());
-addStudentToClass("Jerry");
-console.log(getNumberOfStudents());
