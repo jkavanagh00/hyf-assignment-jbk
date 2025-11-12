7fa17b1d65c1cdb9accae9630e858cb136c2ef01
jkavanagh00 <jkavanagh00@gmail.com>
2025-11-12 17:36:35 +0100

    simplified candyHelper.js

--- courses/foundation/intro-to-javascript/week-2/candyHelper.js
+++ courses/foundation/intro-to-javascript/week-2/candyHelper.js
@@ -2,14 +2,19 @@ const pricePerGram = {
     sweet: 0.5,
     chocolate: 0.7,
     toffee: 1.1,
-    "chewing-gum": 0.03 
+    chewingGum: 0.03
 };
 
-const addCandy = (type, weight) => canBuyMoreCandy() ? boughtCandyPrices.push(pricePerGram[type] * weight) : console.log("Enough candy for you!");
-
 const boughtCandyPrices = [];
-const calculateTotalSpent = () => boughtCandyPrices.length > 0 ? boughtCandyPrices.reduce((x, y) => x + y) : 0 ;
-
-const canBuyMoreCandy = () => amountToSpend > calculateTotalSpent();
-
 const amountToSpend = Math.random() * 100;
+
+function addCandy(type, weight) {
+    const totalSpent = boughtCandyPrices.length > 0 ? boughtCandyPrices.reduce((x, y) => x + y) : 0;
+    const price = pricePerGram[type] * weight;
+    if (totalSpent + price < amountToSpend) {
+        boughtCandyPrices.push(price);
+        console.log("You can buy more, so please do!");
+    } else {
+        console.log("Enough candy for you!");
+    }
+}
