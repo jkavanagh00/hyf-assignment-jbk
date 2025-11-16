ef5d573e4136c2e4f83b397b4ff39bf5f638d3ea
jkavanagh00 <jkavanagh00@gmail.com>
2025-11-16 12:22:34 +0100

    improved variable naming

--- courses/foundation/intro-to-javascript/week-2/candyHelper.js
+++ courses/foundation/intro-to-javascript/week-2/candyHelper.js
@@ -8,11 +8,11 @@ const pricePerGram = {
 const boughtCandyPrices = [];
 const amountToSpend = Math.random() * 100;
 
-function addCandy(type, weight) {
+function addCandy(candyType, grams) {
     const totalSpent = boughtCandyPrices.length > 0 ? boughtCandyPrices.reduce((x, y) => x + y) : 0;
-    const price = pricePerGram[type] * weight;
-    if (totalSpent + price < amountToSpend) {
-        boughtCandyPrices.push(price);
+    const candyPrice = pricePerGram[candyType] * grams;
+    if (totalSpent + candyPrice < amountToSpend) {
+        boughtCandyPrices.push(candyPrice);
         console.log("You can buy more, so please do!");
     } else {
         console.log("Enough candy for you!");
