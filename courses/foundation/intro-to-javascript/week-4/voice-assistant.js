9ea74646ef6751092ca6fad09d46e4b05afb4de6
jkavanagh00 <jkavanagh00@gmail.com>
2025-12-07 12:53:17 +0100

    comitted for new lesson

--- courses/foundation/intro-to-javascript/week-4/voice-assistant.js
+++ courses/foundation/intro-to-javascript/week-4/voice-assistant.js
@@ -3,26 +3,26 @@ let toDo = [];
 
 function introduction(nameString) {
     if (!nameString) {
-        console.log("Invalid name input")
+        return "invalid name input"
     } else if (userName === nameString) {
-        console.log(`Nice to see you again, ${userName}`)
+        return `nice to see you again, ${userName}`
     } else {
         userName = nameString;
-        console.log(`Nice to meet you, ${userName}`)
+        return `nice to meet you, ${userName}`
     }
 }
 
 function confirmName() {
     if (userName.length > 0) {
-        console.log("I don't believe we've met")
+        return `your name is ${userName}, of course`
     } else {
-        console.log(`Your name is ${userName}, of course`)
+        return "i don't believe we've met"
     }
 }
 
 function addToDoItem(str) {
     toDo.push(str);
-    console.log(`Added ${str} to your to-do list`)
+    return `added ${str} to your to-do list`
 }
 
 function removeToDoItem(str) {
@@ -30,69 +30,65 @@ function removeToDoItem(str) {
     if (i > -1) {
         toDo.splice(i, 1);
     }
-    console.log(`Removed ${str} from your to-do list`)
+    return `removed ${str} from your to-do list`
 }
 
 function displayToDo(arr) {
     if (arr.length === 0) {
-        console.log(`You have no items on your To Do list`);
+        return `you have no items on your to do list`
     } else if (arr.length === 1) {
-        console.log(`You have 1 item on your To Do list: ${arr[0]}`);
+        return `you have 1 item on your to do list: ${arr[0]}`
     } else {
         const list = arr.slice(0, arr.length - 1).join(', ') + ` and ${arr[arr.length - 1]}`;
-        console.log(`You have ${toDo.length} items on your To Do list: ${list}`);
+        return `you have ${toDo.length} items on your To Do list: ${list}`
     }
 }
 
 function displayDay() {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const day = new Date().getDay();
-    console.log(`It is currently ${days[day]}`)
+    return `it is currently ${days[day]}`
 }
 
 function add(x, y) {
-    console.log(`${x} + ${y} = ${Number(x) + Number(y)}`)
+    return `${x} + ${y} = ${Number(x) + Number(y)}`
 }
 
 function subtract(x, y) {
-    console.log(`${x} - ${y} = ${Number(x) - Number(y)}`)
+    return `${x} - ${y} = ${Number(x) - Number(y)}`
 }
 
 function divide(x, y) {
-    console.log(`${x} / ${y} = ${Number(x) / Number(y)}`)
+    return `${x} / ${y} = ${Number(x) / Number(y)}`
 }
 
 function multiply(x, y) {
-    console.log(`${x} * ${y} = ${Number(x) * Number(y)}`)
+    return `${x} * ${y} = ${Number(x) * Number(y)}`
 }
 
 function setTimer(mins) {
     const milliseconds = mins * 6000;
-    console.log(`${mins}-minute timer is set`);
     setTimeout(function () {
-        console.log(`${mins}-minute timer is complete`);
+        return `${mins}-minute timer is complete`
     }, milliseconds);
+    return `${mins}-minute timer is set`;
 }
 
 function getReply(command) {
-    const name = command.split(" ")[command.split(" ").length - 1]
     const commandArray = command.toLowerCase().split(" ");
     console.log(`COMMAND [${command}]`);
     if (commandArray[0] === "hello") {
-        introduction(name);
-    } else if (command === "What is my name?") {
+        userName = command.split(" ")[command.split(" ").length - 1];
+        introduction(userName);
+    } else if (command === "what is my name") {
         confirmName();
     } else if (commandArray[0] === "add") {
-        commandArray.splice(0, 1);
-        commandArray.splice(commandArray.length - 3);
-        addToDoItem(commandArray.join(" "));
+        addToDoItem(commandArray[1]);
     } else if (commandArray[0] === "remove") {
-        commandArray.splice(0, 1);
-        commandArray.splice(commandArray.length - 3);
-        removeToDoItem(commandArray.join(" "));
-    } else if (command === "What is on my todo?") {
+        removeToDoItem(commandArray[1]);
+    } else if (command === "what is on my to do") {
         displayToDo(toDo)
-    } else if (command === "What day is it today?") {
+    } else if (command === "what day is it today") {
         displayDay()
     } else if (command.includes('+')) {
         add(commandArray[commandArray.length - 3], commandArray[commandArray.length - 1]);
