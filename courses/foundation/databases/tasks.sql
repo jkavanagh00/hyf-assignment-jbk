67e4f7a8ff727a9f3bf8b8e78f7c47839630f712
jkavanagh00 <jkavanagh00@gmail.com>
2025-12-08 14:35:07 +0100

    created tasks.sql and added a commented list of required queries

---
+++ courses/foundation/databases/tasks.sql
@@ -0,0 +1,19 @@
+-- How many tasks are in the task table?
+
+-- How many tasks in the task table do not have a valid due date?
+
+-- Find all the tasks that are marked as done.
+
+-- Find all the tasks that are not marked as done.
+
+-- Get all the tasks, sorted with the most recently created first.
+
+-- Get the single most recently created task.
+
+-- Get the title and due date of all tasks where the title or description contains database.
+
+-- Get the title and status (as text) of all tasks.
+
+-- Get the name of each status, along with a count of how many tasks have that status.
+
+-- Get the names of all statuses, sorted by the status with most tasks first.
