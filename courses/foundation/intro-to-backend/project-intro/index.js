638d23b24b197c502e64d07cc4c3328cd419cb5f
jkavanagh00 <jkavanagh00@gmail.com>
2026-01-03 13:44:37 +0100

    updated home route to supply styled html

--- courses/foundation/intro-to-backend/project-intro/index.js
+++ courses/foundation/intro-to-backend/project-intro/index.js
@@ -2,7 +2,21 @@ import express from "express";
 const app = express();
 
 app.get("/", (_request, response) => {
-  response.send("hello");
+  response.send(`
+      <h1>Hello!</h1>
+      <h2>This is my first foray into backend</h2>
+      <style>
+      body {
+              background: #ccc;
+      }
+      h1, h2 {
+        text-align: center;
+        color: #fff434;
+        text-shadow: 1px 0 #000, -1px 0 #000, 0 1px #000, 0 -1px #000,
+             1px 1px #000, -1px -1px #000, 1px -1px #000, -1px 1px #000;
+      }
+      </style>
+  `);
 });
 
 app.listen(3000, function () {
