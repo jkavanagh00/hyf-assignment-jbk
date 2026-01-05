cd120d0e0bef1f48705b32f364869d3f2a6c077a
jkavanagh00 <jkavanagh00@gmail.com>
2026-01-05 22:31:48 +0100

    refactored renderProducts() to display data in table form; added functions to format price and rating; applied styling

--- courses/foundation/intro-to-frontend/HYFBay/main.js
+++ courses/foundation/intro-to-frontend/HYFBay/main.js
@@ -3,16 +3,48 @@ console.log("Script loaded");
 const products = getAvailableProducts();
 console.log(products);
 
-// This should create the ul and the li's with the individual products details
-function renderProducts(products) {
-  document.body.innerHTML = "";
-  const list = document.createElement("ul");
+const renderProducts = products => {
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+
   for (let i = 0; i < products.length; i++) {
-    const item = document.createElement("li");
-    item.innerHTML = "<b>" + products[i].name + "</b>" + "<br>" + products[i].price + " kr.<br>rating: " + products[i].rating;
-    list.appendChild(item);
+    const row = document.createElement("tr");
+
+    const name = document.createElement("td");
+    name.innerHTML = "<b>" + products[i].name + "</b>";
+    row.appendChild(name);
+
+    const price = document.createElement("td");
+    price.innerHTML = formatPrice(products[i].price) + " kr.";
+    row.appendChild(price);
+
+    const rating = document.createElement("td");
+    rating.innerHTML = formatRating(products[i].rating);
+    row.appendChild(rating);
+
+    tbody.appendChild(row);
   }
-  document.body.appendChild(list);
 }
 
-renderProducts(products); 
+// star: U+2605; half-star: U+2BE8
+
+const formatPrice = number => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const formatRating = number => {
+  let formattedRating = ""
+  if (number % 2 === 0) {
+    for (let i = 0; i < number; i += 2) {
+      formattedRating = formattedRating + "&#9733 ";
+    }
+  } else {
+    for (let i = 0; i < number-2; i += 2) {
+      formattedRating = formattedRating + "&#9733 ";
+    }
+    formattedRating = formattedRating + "&#11240 ";
+  }
+  return '<span class="stars">' + formattedRating + '</span>';
+}
+
+renderProducts(products);
