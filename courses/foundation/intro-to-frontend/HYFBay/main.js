7649ccb3577ccdc2f9a2abfb41d4d57ef8de5a9e
jkavanagh00 <jkavanagh00@gmail.com>
2026-01-05 17:26:30 +0100

    added HYFbay files (all logic is contained within main.js, including list creation)

--- courses/foundation/intro-to-frontend/HYFBay/main.js
+++ courses/foundation/intro-to-frontend/HYFBay/main.js
@@ -5,7 +5,14 @@ console.log(products);
 
 // This should create the ul and the li's with the individual products details
 function renderProducts(products) {
-  // your code goes here
+  document.body.innerHTML = "";
+  const list = document.createElement("ul");
+  for (let i = 0; i < products.length; i++) {
+    const item = document.createElement("li");
+    item.innerHTML = "<b>" + products[i].name + "</b>" + "<br>" + products[i].price + " kr.<br>rating: " + products[i].rating;
+    list.appendChild(item);
+  }
+  document.body.appendChild(list);
 }
 
 renderProducts(products); 
