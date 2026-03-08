bc82241e52875a56b526afdcbfc3c08d024a6f2e
jkavanagh00 <jkavanagh00@gmail.com>
2026-03-08 13:29:00 +0100

    completed exercise 6

--- courses/backend/advanced-javascript/week1/exercise-6.js
+++ courses/backend/advanced-javascript/week1/exercise-6.js
@@ -1,5 +1,5 @@
 import { teas } from "./data/teas.js";
 
-const displayStrings = teas.map(tea => `${tea.name} - ${tea.pricePerGram * 100}/100g`);
-console.log(teas)
+const displayStrings = teas.map(tea => `${tea.name} - ${tea.pricePerGram * 100} DKK/100g`);
+
 console.log(displayStrings);
