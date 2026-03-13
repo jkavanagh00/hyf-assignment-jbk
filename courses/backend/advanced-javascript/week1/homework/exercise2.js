90dabb5b6c24118e30c655ec36a2e84d87f68a26
jkavanagh00 <jkavanagh00@gmail.com>
2026-03-13 14:12:54 +0100

    fixed calculation of average price to properly return averagePricePerGram

--- courses/backend/advanced-javascript/week1/homework/exercise2.js
+++ courses/backend/advanced-javascript/week1/homework/exercise2.js
@@ -4,14 +4,15 @@ function inventoryReport(teas) {
     const totalTeas = teas.length;
     const inStock = teas.filter(tea => tea.inStock).length;
     const outOfStock = totalTeas - inStock;
-    const totalInventoryValue = teas.map(tea => tea.pricePerGram * tea.stockCount).reduce((x, y) => x + y);
-    const averagePrice = Number((totalInventoryValue / totalTeas).toFixed(2));
+    const totalInventoryValue = teas.map(tea => tea.pricePerGram * tea.stockCount).reduce((x, y) => x + y, 0);
+    const totalPricePerGram = teas.map(tea => tea.pricePerGram).reduce((x, y) => x + y, 0);
+    const averagePricePerGram = Number((totalPricePerGram / totalTeas).toFixed(2));
     return {
         totalTeas,
         inStock,
         outOfStock,
         totalInventoryValue,
-        averagePrice
+        averagePricePerGram
     }
   };
 
