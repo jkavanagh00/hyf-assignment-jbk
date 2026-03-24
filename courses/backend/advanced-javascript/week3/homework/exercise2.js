6410f6e3cc750b54569593f286f42c87694574ca
jkavanagh00 <jkavanagh00@gmail.com>
2026-03-24 15:38:27 +0100

    commented out test for export

--- courses/backend/advanced-javascript/week3/homework/exercise2.js
+++ courses/backend/advanced-javascript/week3/homework/exercise2.js
@@ -1,6 +1,6 @@
 const API_BASE = "https://tea-api-787553294298.europe-west1.run.app/api";
 
-async function getTeaDetails(id) {
+export async function getTeaDetails(id) {
   const [tea, inventory] = await Promise.all([
     fetch(`${API_BASE}/teas/${id}/`).then((result) => result.json()),
     fetch(`${API_BASE}/inventory/${id}`).then((result) => result.json()),
@@ -12,9 +12,9 @@ async function getTeaDetails(id) {
 }
 
 // Test it:
-getTeaDetails(1).then((tea) => {
-  console.log(`${tea.name} (${tea.origin})`);
-  console.log(`Price: ${tea.pricePerGram} DKK/gram`);
-  console.log(`Stock: ${tea.stock} grams`);
-  console.log(`Value: ${(tea.pricePerGram * tea.stock).toFixed(2)} DKK`);
-});
+// getTeaDetails(1).then((tea) => {
+//   console.log(`${tea.name} (${tea.origin})`);
+//   console.log(`Price: ${tea.pricePerGram} DKK/gram`);
+//   console.log(`Stock: ${tea.stock} grams`);
+//   console.log(`Value: ${(tea.pricePerGram * tea.stock).toFixed(2)} DKK`);
+// });
